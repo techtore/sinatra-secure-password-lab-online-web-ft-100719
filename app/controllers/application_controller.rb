5e1b7bdfaaddc1d7a28c430b695c9b3caa909716
Victoria Hale <vic.hale74@gmail.com>
2019-11-22 19:01:18 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,7 +37,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    user = User.find)by(:username => params[:username], :password => params[:password])
+    user = User.find_by(:username => params[:username], :password => params[:password])
     if user && user.authenticate(params[:password])
       erb :account
     else 
