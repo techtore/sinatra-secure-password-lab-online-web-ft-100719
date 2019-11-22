29eb50789a35be80b0b73bc52833f12c52161fa6
Victoria Hale <vic.hale74@gmail.com>
2019-11-22 18:56:56 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,9 +17,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    user = User.new(:username => params[:username], :password => params[:password])
-   
-    if user.save
+    if user = User.create(:username => params[:username], :password => params[:password])
       redirect "/login"
     else params[:username] == "" || params[:password] == ""
       redirect "/failure"
